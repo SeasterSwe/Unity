b7c425c0a25fe0f40129fbf2287837b1490898ab
SeasterSwe <Jakobbulow01@gmail.com>
2021-01-13 16:28:03 +0100

    HighScoreList with playerprefs
    Remains to save the score after a game

---
+++ InputLesson00/Assets/ScoreLerp.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class ScoreLerp : MonoBehaviour
+{
+    float targetScore = 200;
+    float displayScore = 0f;
+    private TextMeshProUGUI text;
+    RectTransform rectTransform;
+    public float endScale = 1.5f;
+    public float startScale = 0.5f;
+    public float effectSpeed;
+    void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+        rectTransform = GetComponent<RectTransform>();
+
+        StartCoroutine(LerpScore(targetScore, effectSpeed));
+        
+    }
+    IEnumerator LerpScore(float targetScore, float speed)
+    {
+        float totalTime = 1f / speed;
+        float lerpVal = 0f;
+        rectTransform.localScale = Vector3.one * startScale;
+        rectTransform.DOScale(Vector3.one * endScale, totalTime + speed);
+        while (lerpVal <= 1f)
+        {
+            lerpVal += Time.deltaTime * speed;
+
+            displayScore = Mathf.Lerp(0, targetScore, lerpVal);
+            text.text = Mathf.RoundToInt(displayScore).ToString();
+            yield return null;
+        }
+    }
+}
