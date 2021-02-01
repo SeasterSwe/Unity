c8839ae5cde46d96b9bdabdf420e2e99e82ecc15
SeasterSwe <Jakobbulow01@gmail.com>
2021-02-01 15:18:15 +0100

    Coockies per sec text

---
+++ CoockieKlicker/Assets/CPSDisplayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class CPSDisplayer : MonoBehaviour
+{
+    TextMeshProUGUI text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string text = ($"CPS : {ScoreManager.CPS}");
+        this.text.text = text;
+    }
+}
